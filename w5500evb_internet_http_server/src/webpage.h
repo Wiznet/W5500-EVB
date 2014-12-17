d97446ea877923dd91f2bdd5d57e2a3deed3f074
Eric Jung <myst5829@gmail.com>
2014-12-17 09:25:57 +0900

    Example HTML modified
    Examples more easier

--- w5500evb_internet_http_server/src/webpage.h
+++ w5500evb_internet_http_server/src/webpage.h
@@ -3,7 +3,6 @@
 
 extern uint8_t index_page_array[];
 
-/* AJAX JavaScript Example */
 #define wiz550web_ajax_js 	"function AJAX(a,e){var c=d();c.onreadystatechange=b;function d(){if(window.XMLHttpRequest){return new XMLHttpRequest()}else{if(window.ActiveXObject){return new ActiveXObject(\"Microsoft.XMLHTTP\")}}}function b(){if(c.readyState==4){if(c.status==200){if(e){e(c.responseText)}}}}this.doGet=function(){c.open(\"GET\",a,true);c.send(null)};this.doPost=function(f){c.open(\"POST\",a,true);c.setRequestHeader(\"Content-Type\",\"application/x-www-form-urlencoded\");c.setRequestHeader(\"ISAJAX\",\"yes\");c.send(f)}}function $(a){return document.getElementById(a)}function $$(a){return document.getElementsByName(a)}function $$_ie(a,c){if(!a){a=\"*\"}var b=document.getElementsByTagName(a);var e=[];for(var d=0;d<b.length;d++){att=b[d].getAttribute(\"name\");if(att==c){e.push(b[d])}}return e}"
 
 /* Get: Digital I/O state/dir:  function DioCallback(o), getDio(o) */
@@ -39,7 +38,7 @@ extern uint8_t index_page_array[];
 
 /* Get: Analog Input: 	function AinCallback(o), getAin(o) */
 /* Additional function: function AinDrawgraph(o) */
-#define wiz550web_ain_js 	"function AinCallback(o){"\
+#define wiz550web_ain_js	"function AinCallback(o){"\
 								"var pin = o.ain_p;"\
 								"$('txtain_v'+pin).value=o.ain_v;"\
 								"AinDrawgraph(o);"\
@@ -65,7 +64,7 @@ extern uint8_t index_page_array[];
 							"}"
 
 
-#define dio_page			"<!DOCTYPE html>"\
+#define dio_page  			"<!DOCTYPE html>"\
 							"<html>"\
 								"<head>"\
 								"<title>W5500-EVB Web Server Digital I/O</title>"\
@@ -75,13 +74,15 @@ extern uint8_t index_page_array[];
 								"</head>"\
 								"<body>"\
 									"<div>"\
-									">> todo"\
+									"<input type='button' value='LED R On' pin='10' s='0' onclick='setDiostate(this);'> "\
+									"<input type='button' value='LED R Off' pin='10' s= '1' onclick='setDiostate(this);'>"\
+									"<!--to do-->"\
 									"</div>"\
 								"</body>"\
 							"</html>"
 
 
-#define ain_page			"<!DOCTYPE html>"\
+#define ain_page  			"<!DOCTYPE html>"\
 							"<html>"\
 								"<head>"\
 								"<title>W5500-EVB Web Server Analog Input</title>"\
@@ -97,7 +98,9 @@ extern uint8_t index_page_array[];
 								"</head>"\
 								"<body>"\
 									"<div>"\
-									">> todo"\
+									"<!--to do-->"\
+									"<br>"\
+									"<div class='analog' style='padding:0px;'><strong id='ain_v6' name='ain' class='ain' style='width:0%;'></strong></div>"\
 									"</div>"\
 								"</body>"\
 							"</html>"
